c3d85be352c8752b2b6e0f609276be3918268d88
Deepak <deepakkumar750206@gmail.com>
2023-02-23 19:39:23 +0530

    Create separate module and export anonyms function from there

--- Node-Farm/index.js
+++ Node-Farm/index.js
@@ -3,6 +3,7 @@ const http = require("http");  //"http" -> handle the request and response insid
                                // http give us networking capability such as building an http server
 
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate')
 
 // ////////////////////////////////////
 // ////    FILE SYSTEM
@@ -38,21 +39,6 @@ const url = require('url');
 
 ////////////////////////////////////
 ///// SERVER
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    }
-    return output;
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
