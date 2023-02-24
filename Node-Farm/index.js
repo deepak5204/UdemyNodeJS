d96d90c1c02a0173b6d1ce1b423f9b6813a4829a
Deepak <deepakkumar750206@gmail.com>
2023-02-24 12:40:36 +0530

    Use 3rd party module slugify

--- Node-Farm/index.js
+++ Node-Farm/index.js
@@ -3,6 +3,7 @@ const http = require("http");  //"http" -> handle the request and response insid
                                // http give us networking capability such as building an http server
 
 const url = require('url');
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate')
 
 // ////////////////////////////////////
@@ -48,8 +49,11 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
     const dataObj = JSON.parse(data);
 
-const server = http.createServer((req, res) => {
+    const slugs = dataObj.map(el => slugify(el.productName, {lower: true})); 
+    console.log(slugs);
+    // console.log(slugify('Fresh Avocados', {lower: true}));
 
+const server = http.createServer((req, res) => {
     //ES-6 destructuring
     const {query, pathname} = (url.parse(req.url, true))
     
