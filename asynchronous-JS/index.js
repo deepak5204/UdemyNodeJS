3adbe169924f0ad5a154a26f18166bc6d35f18f9
Deepak <deepakkumar750206@gmail.com>
2023-02-25 15:43:55 +0530

    Data not fetch from dog API

---
+++ asynchronous-JS/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+
+  superagent
+  .get(`https://dog.ceo/api/breeds/${data}/image/random`)
+  .end((err, res) => {
+    if(err) return console.log(err.message);
+    console.log(res.body.message);
+  });
+});
