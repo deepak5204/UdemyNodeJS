0327773f5f2b46ca39b7219684ad63c5452606a2
Deepak <deepakkumar750206@gmail.com>
2023-02-25 16:12:54 +0530

    data not getting from dog API

--- asynchronous-JS/index.js
+++ asynchronous-JS/index.js
@@ -5,9 +5,13 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
   superagent
-  .get(`https://dog.ceo/api/breeds/${data}/image/random`)
-  .end((err, res) => {
-    if(err) return console.log(err.message);
-    console.log(res.body.message);
-  });
+    .get(`https://dog.ceo/api/breeds/${data}/image/random`)
+    .end((err, res) => {
+      if (err) return console.log(err.message);
+      console.log(res.body.message);
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log('Random dog image saved to file!');
+      });
+    });
 });
