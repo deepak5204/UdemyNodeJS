b24d579284bb448c2f9569d1b02aa498d79b0835
Deepak <deepakkumar750206@gmail.com>
2023-02-26 18:45:32 +0530

    Create new tour using Post route and write inside file

--- Natours/app.js
+++ Natours/app.js
@@ -1,7 +1,9 @@
-const fs = require('fs')
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+app.use(express.json()); // here "express.json()" is middlware and middleware is just a function that modify the incoming request
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({
@@ -16,23 +18,41 @@ const app = express();
 //   });
 // });
 
-const tours = JSON.parse( 
+const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
-      tours
-    }
+      tours,
+    },
   });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
 
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
 
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
 const port = 3000;
 app.listen(port, () => {
