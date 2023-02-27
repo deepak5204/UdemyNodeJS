2c348bc1c1ea26dd7701760034751d5fc8b9a3aa
Deepak <deepakkumar750206@gmail.com>
2023-02-27 17:51:09 +0530

    Delete route

--- Natours/app.js
+++ Natours/app.js
@@ -92,6 +92,22 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+// delete method
+app.delete('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params.id);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id!',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
