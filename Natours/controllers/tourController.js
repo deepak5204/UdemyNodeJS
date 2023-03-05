645387582dc4c27b27a579a5f08c0082bf571cb9
Deepak <deepakkumar750206@gmail.com>
2023-03-05 09:51:47 +0530

    Param middleware used for check idis valid or not

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -3,6 +3,17 @@ const fs = require('fs');
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
   );
+
+  exports.checkID = (req, res, next, val) => {
+    console.log(`Tour id is:${val}`);
+    if(req.params.id * 1 > tours.length){
+      return res.status(404).json({
+        status: "Fail",
+        message: "Invalid ID"
+      });
+    }
+    next();
+  };
   
 exports.getAllTours = (req, res) => {
     res.status(200).json({
@@ -19,13 +30,7 @@ exports.getTour = (req, res) => {
     const id = req.params.id * 1;
     const tour = tours.find((el) => el.id === id);
   
-    // if(id > tours.length){
-    if (!tour) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid Id!',
-      });
-    }
+  
   
     res.status(200).json({
       status: 'success',
@@ -57,12 +62,7 @@ exports.createTour = (req, res) => {
   
 exports.updateTour = (req, res) => {
     console.log(req.params.id);
-    if (req.params.id * 1 > tours.length) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid Id!',
-      });
-    }
+   
     res.status(200).json({
       status: 'success',
       data: {
@@ -73,12 +73,7 @@ exports.updateTour = (req, res) => {
   
 exports.deleteTour = (req, res) => {
     console.log(req.params.id);
-    if (req.params.id * 1 > tours.length) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid Id!',
-      });
-    }
+   
     res.status(200).json({
       message: 'deleted Successfully',
     });
