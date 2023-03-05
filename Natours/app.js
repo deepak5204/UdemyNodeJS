13b2a70e67381aeaca30d74eaabf8bc7af348a32
Deepak <deepakkumar750206@gmail.com>
2023-03-05 19:03:04 +0530

    configure eslint and prettierrc file

--- Natours/app.js
+++ Natours/app.js
@@ -1,21 +1,20 @@
-
+// /* eslint-disable prettier/prettier */
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // 1) MIDDLEWARE
 // console.log(process.env.NODE_ENV);
 
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-// app.use(morgan('dev'));
 app.use(express.json()); // here "express.json()" is middlware and middleware is just a function that modify the incoming request
-app.use(express.static(`${__dirname}/public/`));//static file
+app.use(express.static(`${__dirname}/public/`)); //static file
 
 //create own middleware
 app.use((req, res, next) => {
@@ -27,7 +26,6 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
 
 // 3) ROUTE
 
--- Natours/server.js
+++ Natours/server.js
@@ -2,9 +2,11 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env'});
 
 const app = require('./app');
-// console.log(process.env);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+const x = 26;
+x=66;
