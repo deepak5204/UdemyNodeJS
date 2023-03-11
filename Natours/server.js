6115fb7b373cce946676b242e992362d8c8d70fa
Deepak <deepakkumar750206@gmail.com>
2023-03-11 08:21:03 +0530

    create schema and model for tour data

--- Natours/server.js
+++ Natours/server.js
@@ -4,7 +4,36 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env'});
 
-mongoose.connect();
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>', 
+  process.env.DATABASE_PASSWORD);
+
+mongoose
+.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => { 
+   console.log('DB connection successfull');
+});
+
+const tourSchema = new mongoose.Schema({
+  name:{ 
+    type: String,
+    unique: true,
+    required: [true, 'A tour must have anname']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
