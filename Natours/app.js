4d78dd3a0fe3349b26380acc4c21f965b6282b83
Deepak <deepakkumar750206@gmail.com>
2023-03-16 21:33:48 +0530

    create user friendly json error whenever http request with wrong url

--- Natours/app.js
+++ Natours/app.js
@@ -17,10 +17,10 @@ app.use(express.json()); // here "express.json()" is middlware and middleware is
 app.use(express.static(`${__dirname}/public/`)); //static file
 
 //create own middleware
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware!');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -31,5 +31,12 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) =>{
+  res.status(404).json({
+    status:'fail',
+    message: `cant find ${req.originalUrl} on this server!`
+  });
+});
 
 module.exports = app;
