82f695d7567a931d7d5672d38042c41360de1486
Deepak <deepakkumar750206@gmail.com>
2023-03-16 21:56:53 +0530

    Implement Global error handling

--- Natours/app.js
+++ Natours/app.js
@@ -33,10 +33,26 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) =>{
-  res.status(404).json({
-    status:'fail',
-    message: `cant find ${req.originalUrl} on this server!`
-  });
+  // res.status(404).json({
+  //   status:'fail',
+  //   message: `cant find ${req.originalUrl} on this server!`
+  // });
+
+  const err = new Error(`cant find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
 });
+
+//ERROR HANDLING MIDDLEWARE (GLOBAL ERROR HANDLING)
+app.use((err, req, res, next) => {
+  err.satatusCode =  err.statusCode || 500;
+  err.status = err.status || 'error'
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  }) 
+})
 
 module.exports = app;
