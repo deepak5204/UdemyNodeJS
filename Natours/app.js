c66fe1ffb0e8fb43d3dc00d3ea256c3c943737fc
Deepak <deepakkumar750206@gmail.com>
2023-03-17 21:36:56 +0530

    Refactor globalErrorHandler middleware function and move inside errorController.js file

--- Natours/app.js
+++ Natours/app.js
@@ -39,13 +39,6 @@ app.all('*', (req, res, next) =>{
 });
 
 //ERROR HANDLING MIDDLEWARE (GLOBAL ERROR HANDLING)
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
