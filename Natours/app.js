aa261e06c371b046db016fcb05bd00883d5b7a72
Deepak <deepakkumar750206@gmail.com>
2023-03-17 21:32:47 +0530

    Update Global error handling middleware

--- Natours/app.js
+++ Natours/app.js
@@ -35,13 +35,7 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) =>{
-
-  const err = new Error(`cant find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
-
-  // next(new AppError(`cant find ${req.originalUrl} on this server!`, 404));
+  next(new AppError(`cant find ${req.originalUrl} on this server!`, 404));
 });
 
 //ERROR HANDLING MIDDLEWARE (GLOBAL ERROR HANDLING)
--- Natours/utils/appError.js
+++ Natours/utils/appError.js
@@ -1,11 +1,12 @@
 class AppError extends Error {
-    constructor(message, statusCode) {
-        super(message);
+    constructor(message, statusCode) {  //call each time whenever create an object
+        super(message); //call parent constructor(); by doind this set message property to our incoming message
 
         this.statusCode = statusCode;
         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
         this.isOperational = true;
 
+        //stackTrace - where error happen
         Error.captureStackTrace(this, this.constructor);
     }
 }
