663997fe23d104b6447ed9457415505d7bfa2538
Deepak <deepakkumar750206@gmail.com>
2023-03-20 20:01:00 +0530

    Distinguish between operational error and other error and send message according to them

--- Natours/controllers/errorController.js
+++ Natours/controllers/errorController.js
@@ -8,11 +8,25 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-}
+  //Operational error, trusted error: send message to client 
+  if(err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } 
+  
+  //Programming or other unknown error: don't leak error details
+  else {
+    // 1). Log error
+    console.error('ERROR***', err);
+    
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  };
+};
 
 
 module.exports = (err, req, res, next) => {
