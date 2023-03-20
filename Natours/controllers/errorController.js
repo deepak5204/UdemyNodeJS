32d5f67a3096b927e7e6ca57c1a0a7452723b952
Deepak <deepakkumar750206@gmail.com>
2023-03-20 19:49:35 +0530

    send different error during production and development

--- Natours/controllers/errorController.js
+++ Natours/controllers/errorController.js
@@ -1,10 +1,30 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+}
+
+
 module.exports = (err, req, res, next) => {
     // console.log(err.stack); //err.stack basically show where the error happen
 
       err.statusCode =  err.statusCode || 500;
       err.status = err.status || 'error'
-      res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-      }) 
+
+      if(process.env.NODE_ENV === 'development'){
+        sendErrorDev();
+      }
+      else if(process.env.NODE_ENV === 'production'){
+        sendErrorProd();
+      } 
     }
