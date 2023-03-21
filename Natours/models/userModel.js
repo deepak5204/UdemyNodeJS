bc4e2c6911e4c6929e674e782e662b49f9d759a9
Deepak <deepakkumar750206@gmail.com>
2023-03-21 21:21:22 +0530

    Install bcryptjs package and password encrypt using bcrypt.hash() function

--- Natours/models/userModel.js
+++ Natours/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const validator = require('validator')
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -30,7 +31,21 @@ const userSchema = new mongoose.Schema({
             },
             message: 'Password are not same!'
         }
-    },
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    //only run this function if password was actually modified
+    if(!this.isModified('password')){
+        return next();
+    }
+
+    //Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12); //encrypt the password
+
+    //delete passwordConfirm field 
+    this.passwordConfirm = undefined; 
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
