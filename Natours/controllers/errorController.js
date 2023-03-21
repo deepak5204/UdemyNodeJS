a245d6311b03ceb4cbd52506a15623f58f8abb8b
Deepak <deepakkumar750206@gmail.com>
2023-03-21 06:23:49 +0530

    Handle error for wrong ID request

--- Natours/controllers/errorController.js
+++ Natours/controllers/errorController.js
@@ -1,3 +1,11 @@
+const AppError = require('./../utils/appError');
+
+const handleCadtErrorDB = err => {
+  const message = 'Invalid ${err.path}: ${err.value}.'
+  return new AppError(message, 400);
+};
+
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -39,6 +47,9 @@ module.exports = (err, req, res, next) => {
         sendErrorDev(err, res);
       }
       else if(process.env.NODE_ENV === 'production'){
-        sendErrorProd(err, res);
+        let error = {...err };
+        if(error.name === 'CastError') 
+        error = handleCadtErrorDB(error)
+        sendErrorProd(error, res);
       } 
     }
