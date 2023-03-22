5efa98345cb6cc3a230e85108585708735c947b6
Deepak <deepakkumar750206@gmail.com>
2023-03-22 10:17:07 +0530

    Hide password from all user responce

--- Natours/controllers/userController.js
+++ Natours/controllers/userController.js
@@ -1,12 +1,19 @@
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
 
 
+exports.getAllUsers = catchAsync (async (req, res, next) => {
+  const users = await User.find();
 
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: "This route is not yet defined!"
-    });
-  };
+  //SEND RESPONSE
+      res.status(200).json({
+        status: 'success',
+        results: users.length, //number of results in users 
+        data: {
+          users,
+        },
+      });
+  });
   
   exports.createUser = (req, res) => {
     res.status(500).json({
