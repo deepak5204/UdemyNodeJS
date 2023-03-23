6e93caa8b620f3e9d416cdc4f4bbe87b9f629a86
Deepak <deepakkumar750206@gmail.com>
2023-03-23 21:57:17 +0530

    Write methods for protect gatAllTour routes

--- Natours/controllers/authController.js
+++ Natours/controllers/authController.js
@@ -55,3 +55,27 @@ exports.login = catchAsync (async (req, res,next) => {
         token
     });
 });
+
+
+//Protect Get all routes
+exports.protect = catchAsync(async (req, res, next) => {
+    // 1) Getting token and check of it's there
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+       token = req.headers.authorization.split(' ')[1]; 
+    }
+    console.log(token);
+
+    if(!token){
+        return next(
+            new AppError('You are not logged in! Please log in to access.', 401)
+        );
+    }
+    
+    // 2) Verification token
+
+    // 3) Check if user still exists
+
+    // 4) Check if user change password after the taken was issued
+    next();
+})
