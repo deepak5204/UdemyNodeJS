7b5b04dd7388498c0508944f44b1c795ec978ba2
Deepak <deepakkumar750206@gmail.com>
2023-03-24 21:18:52 +0530

    create user login access

--- Natours/routes/tourRoutes.js
+++ Natours/routes/tourRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
-const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
@@ -19,7 +18,7 @@ router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
-  .get(authController.protect, tourController.getAllTours)
+  .get( tourController.getAllTours)
   .post(tourController.createTour);
 
 router
